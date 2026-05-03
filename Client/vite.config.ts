1d74378f0d561e8756610fbd1fc30404b76b856e
199520gayu-ops <199520gayu@gmail.com>
2026-05-03 22:10:37 +0530

    add: deploy

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -2,13 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    conditions: ['style', 'import', 'module', 'browser'],
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 })
